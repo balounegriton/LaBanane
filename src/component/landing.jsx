ef970dfd0a74a1b3b2a50097762bb9d6cc890914
balounegriton <provostnoe@gmail.com>
2025-09-11 10:09:20 -0400

    llo2`

--- src/component/landing.jsx
+++ src/component/landing.jsx
@@ -7,47 +7,65 @@ import ReactPlayer from 'react-player';
 function Landing() {
   const [showModal, setShowModal] = useState(false);
 
-  const handlePageClick = () => {
+  // Ouvre le popup
+  const handleOpenPopup = () => {
     setShowModal(true);
   };
 
-  const closeModal = () => {
+  // Ferme le popup
+  const closePopup = () => {
     setShowModal(false);
   };
 
   return (
-    <div onClick={handlePageClick} className="landing">
+    <div className="landing" onClick={handleOpenPopup}>
+      {/* Logo */}
       <div className="logoContainer">
         <img className="logoLanding" id="logo" src={logo} alt="logo" />
       </div>
-
+      
+      {/* Texte */}
       <div className="logoContainer">
         <div className="textePostprod">Production | Post-production</div>
       </div>
-
+      
+      {/* Video en fond */}
       <div className="videoPlayer">
         <video src={video} autoPlay loop muted playsInline></video>
       </div>
-
-      {showModal && (
-        <div className="modal-backdrop" onClick={closeModal}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            {/* Bouton fermer */}
-            <button className="close-btn" onClick={closeModal}>X</button>
-
-            {/* ReactPlayer Vimeo */}
-            <div className="player-wrapper" onClick={(e) => e.stopPropagation()}>
-              <ReactPlayer
-                className="react-player"
-                url="https://vimeo.com/668244502"
-                width="100%"
-                height="100%"
-                controls
-              />
-            </div>
-          </div>
-        </div>
-      )}
+      
+      {/* Bouton mobile centré en haut */}
+      <button 
+        className="popup-toggle-btn"
+        onClick={(e) => {
+          e.stopPropagation(); // empêche la fermeture
+          handleOpenPopup();
+        }}>
+        Voir Demo Reel
+      </button>
+      
+      {/* Modal */}
+{showModal && (
+  <div className="modal-backdrop" onClick={closePopup}>
+    <div
+      className="modal-content"
+      onClick={(e) => e.stopPropagation()} // empêche la fermeture si clic dans le contenu
+    >
+      {/* Bouton fermer */}
+      <button className="close-btn" onClick={closePopup}>X</button>
+      
+      {/* Zone du lecteur Vimeo */}
+      <div className="player-wrapper" onClick={(e) => e.stopPropagation()}>
+        <ReactPlayer
+          url="https://vimeo.com/668244502"
+          controls
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </div>
+  </div>
+)}
     </div>
   );
 }
